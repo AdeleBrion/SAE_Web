ec86060f57492f0e177bd8cd33e3bcfff2dca81c
AdeleBrion <adele.brion@etu.univ-orleans.fr>
2024-01-25 12:26:18 +0100

    debut navbar

---
+++ nav.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="nav.css">
+<link rel="icon" href="fixtures/images/logoBarre.png" type="image/png">
+<nav class="navbar navbar-default navbar">
+    <img class="logo" src="fixtures/images/logo.png">
+    <input type="text" placeholder="Search..">
+    <a href=""><img class="user" src="fixtures/images/user.png"></a>
+</nav>
