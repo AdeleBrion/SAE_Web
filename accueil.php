6f5ae7d0ab44cf2e696e272b4b2ae89e47dde584
AdeleBrion <adele.brion@etu.univ-orleans.fr>
2024-01-28 08:34:01 +0100

    css accueil

--- accueil.php
+++ accueil.php
@@ -10,10 +10,11 @@ require_once "Classes/albumNomImage.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/accueil.css">
+    <title>Mus'inEar</title>
 </head>
 <body>
-<main>
+    <main>
         <section class="albums">
             <h2 class="titre"><img src="fixtures/images/line.png"> Albums <img src="fixtures/images/line.png"></h2>
             <?php
