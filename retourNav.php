4e8a7a960b33983e7ed87a4f86b0d8843af4c14a
TomDubois <tom.dubois@etu.univ-orleans.fr>
2024-02-05 16:56:25 +0100

    renommage accueil->index + insert genreAlbum et StylesMusicaux

--- retourNav.php
+++ retourNav.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="css/retourNav.css">
 <link rel="icon" href="fixtures/images/logoBarre.png" type="image/png">
 <nav>
-    <a href="accueil.php"><img src="fixtures/images/retour.png"></a>
+    <a href="index.php"><img src="fixtures/images/retour.png"></a>
 </nav>
