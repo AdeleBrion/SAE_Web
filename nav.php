a4ac8c8e1d635289115c29545ff01caef497a6f4
TomDubois <tom.dubois@etu.univ-orleans.fr>
2024-02-08 12:17:56 +0100

    debut navbar

--- nav.php
+++ nav.php
@@ -1,7 +1,14 @@
 <link rel="stylesheet" href="css/nav.css">
 <link rel="icon" href="fixtures/images/logoBarre.png" type="image/png">
+<script src="js/menu.js"></script>
+
 <nav class="navbar navbar-default navbar">
     <img class="logo" src="fixtures/images/logo.png">
     <input type="text" placeholder="Search..">
-    <a href=""><img class="user" src="fixtures/images/user.png"></a>
+    <a href="#" id="userBtn"><img class="user" src="fixtures/images/user.png"></a>
+    <div id="menu" class="menu">
+        <a href="#">Option 1</a>
+        <a href="#">Option 2</a>
+        <a href="#">Option 3</a>
+    </div>
 </nav>
