7cfdb73a10a1f2b84e6933f0209699f9ba73bc11
AdeleBrion <adele.brion@etu.univ-orleans.fr>
2024-02-15 10:42:20 +0100

    ajout autoload

---
+++ autoload/Autoloader.php
@@ -0,0 +1,14 @@
+<?php
+namespace autoload;
+
+class Autoloader{
+
+    static function register(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    static function autoload($class){
+        $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        require_once $path . '.php';
+    }
+}
